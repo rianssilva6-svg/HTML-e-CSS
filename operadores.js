c988f5d6e1fb9985aa4f1213ada9b0d391d4f203
rianssilva6-svg <rian.s.silva6@ba.estudante.senai.br>
2026-03-05 16:51:46 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,18 @@
+let idade = 10
+let peso = "10"
+
+console.log("- COMPARAÇÃO -")
+console.log("- Comparando conteudo: ")
+console.log(idade == peso)
+
+console.log("\n- Comparando tipo e conteudo: ")
+console.log(idade === peso)
+
+console.log("\n - DIFERENÇA -")
+console.log("- Comparando conteudo: ")
+console.log("- Idade e peso são diferentes? -")
+console.log(idade != peso)
+
+console.log("\n- Comparando tipo e conteudo: ")
+console.log("- Idade e peso são diferentes? -")
+console.log(idade !== peso)
