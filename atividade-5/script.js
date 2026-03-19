fbc5be5c52dde9717ee55e0042e291a5ae50b5d8
rianssilva6-svg <rian.s.silva6@ba.estudante.senai.br>
2026-03-19 16:45:15 -0300

    Macas

---
+++ atividade-5/script.js
@@ -0,0 +1,35 @@
+function quant() {
+
+    const numeroInput1 = document.getElementById("numeroInput1");
+    let quantidade = parseInt(numeroInput1.value);
+
+    const resultadoDiv = document.getElementById("Valorgerado");
+    resultadoDiv.innerHTML = "";
+
+    if (isNaN(quantidade) || quantidade <= 0) {
+        resultadoDiv.innerHTML = "⚠️ Digite uma quantidade válida!";
+        return;
+    }
+
+    let preco;
+    let total;
+
+    // Regra de preço
+    if (quantidade < 12) {
+        preco = 1.30;
+    } else {
+        preco = 1.00;
+    }
+
+    // Cálculo do total
+    total = quantidade * preco;
+
+    // Exibir resultado com 2 casas decimais
+    resultadoDiv.innerHTML += `<strong>Valor: ${preco.toFixed(2)}</strong>`;
+
+    resultadoDiv.innerHTML += `<strong>Valor da compra: R$ ${total.toFixed(2)}</strong>`;
+}
+
+// Evento no botão correto
+const botao = document.getElementById("Valor");
+botao.addEventListener("click", quant);
