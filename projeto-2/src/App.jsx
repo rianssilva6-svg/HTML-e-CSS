ed8823a679b9e490bccb257181c4eaaaa8583083
rianssilva6-svg <rian.s.silva6@ba.estudante.senai.br>
2026-03-26 16:54:13 -0300

    projeto

---
+++ projeto-2/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import Mensagem from './components/Mensagem'
+
+function App() {
+
+  return (
+    <>
+      <h1>Bem-vindo ao meu projeto</h1>
+
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+
+    </>
+  )
+}
+
+export default App
