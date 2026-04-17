b451446fa68ee0aca74f703f813d78b5d7f6d658
Rian Souza <rian.s.silva6@ba.estudante.senai.br>
2026-04-17 12:27:24 -0300

    Atividade concluída

--- Projeto-atividade/src/App.jsx
+++ Projeto-atividade/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import AdicionarTime from './components/AdicionarTime/Index'
-import AgenteIA from './components/Agente-IA'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
@@ -12,7 +11,6 @@ function App() {
         <main>
           <AdicionarTime/>
         </main>
-        <AgenteIA/>
         <Footer/>
       </div>
     </>
